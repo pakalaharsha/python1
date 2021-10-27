0ae71c465b0e8d8a9428d9893962d5e375224471
pakalaharsha <92503367+pakalaharsha@users.noreply.github.com>
2021-10-27 18:21:35 +0530

    Add files via upload

---
+++ 100.py
@@ -0,0 +1,4 @@
+name=("harsha")
+age=22
+year=str(2021-age+100)
+print(name +"when 100 years old "+year)
---
+++ hs.py
@@ -0,0 +1,10 @@
+x=100
+y=500
+z=300
+if(x>y):
+    print("x is bigger number")
+elif(y>z):
+    print("y is bigger number")
+elif(z>x):
+    print("z is bigger number")
+
